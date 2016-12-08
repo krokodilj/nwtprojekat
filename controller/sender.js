2df59afd9e816f2fdf7fe9ef733c3112ba06dceb
miodragvilotijevic <miodragvilotijevic@gmail.com>
2016-12-08 23:52:24 +0100

    slanje mejlova o gresci svim user-ima koji su prijavljeni na aplikaciju

--- controller/sender.js
+++ controller/sender.js
@@ -11,24 +11,33 @@ var transport = nodemailer.createTransport("SMTP", {
 
 var Sender = {
     
-    send: function (type, obj, user) {
+    send: function (type, obj, users) {
         if(type=='subscribe'){
-            // setup e-mail data with unicode symbols
+            // setup e-mail data 
             var mailOptions = {
-                from: '"errorMonitor', // sender address
-                to: user.email, // list of receivers
+                from: 'errorMonitor 👥', // sender address
+                to: users.email, // list of receivers
                 subject: 'Subcribe on application', // Subject line
                 text: 'Hi, you are subscribed on application '+ obj.name, // plaintext body
-                html: '<p>Hi,</p>'+ // html body
-                     +'<p>you are subcribed on application by '+obj.admin.email+'</p>'+
-                     +'<p>Name:'+obj.name+'</p>'+
-                     +'<p>Stack:'+obj.stack+'</p>'+
-                     +'<p>Version:'+obj.version+'</p>'+
-                     +'<p>Repository:'+obj.repo+'</p>'
+                html: '<p>Hi,</p><p>you are subcribed on application by '.concat(obj.admin.email,'</p><p>Name: ',obj.name,
+                     '</p><p>Stack: ',obj.stack,'</p><p>Version: ',obj.version,'</p>')
             };
 
-        }else if(type=='erroro'){
-            
+        }else if(type=='error'){
+
+            var receivers=users[0];
+            for (i = 1; i < users.length; i++) { 
+                receivers += ', '+users[i];
+            }
+            // setup e-mail data 
+            var mailOptions = {
+                from: 'errorMonitor 👥', // sender address
+                to: receivers, 
+                subject: 'Error on application '+obj.app.name, // Subject line
+                text: 'Date: '+ obj.date, // plaintext body
+                html: '<p>Hi,</p><p>this is notification for error on application '.concat(obj.app.name,'</p><p>Stack      : ',
+                        obj.stack,'</p><p>Fragment   : ',obj.fragment,'</p><p>Date       : ',obj.date,'</p>')
+            };
         }
         transport.sendMail(mailOptions, function(error, info){
             if(error){
