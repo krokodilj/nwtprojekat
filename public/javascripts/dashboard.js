47a09f1c86251fc2b9c160378448438e6ab3776a
C0mpy <n.zunic@yahoo.com>
2017-01-13 19:48:03 +0100

    finished apps view and selection

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,15 +1,17 @@
-(function () {
+(function() {
     angular.module("myApp")
         .controller('dashboardController', dashboardController);
 
     function dashboardController($scope, $http, $cookies) {
 
         var vm = this;
+
         vm.selectedIndex = 0; // index of selected tab in view
+        vm.selectApp = selectApp;
         $scope.indexCtrl.loggedIn = true; //user is logged in set to true
-        var user = $cookies.getObject("userdata");
         console.log($cookies.getAll())
-        $http.post("/users/dashboard", user, { headers: { 'x-access-token': $cookies.get("token") } }).then(function (response) {
+
+        $http.post("/users/dashboard", $cookies.getObject("userdata"), { headers: { 'x-access-token': $cookies.get("token") } }).then(function(response) {
 
             $cookies.putObject("userdata", response.data.userdata);
             $cookies.putObject("subscribed_apps", response.data.subscribed_apps);
@@ -18,7 +20,14 @@
             vm.userdata = response.data.userdata;
             vm.subscribed_apps = response.data.subscribed_apps;
             vm.admin_apps = response.data.admin_apps;
-            
+
         });
+
+        function selectApp(appId) {
+            console.log(appId);
+            vm.selectedAppId = appId;
+            vm.selectedIndex = 1;
+        }
+
     }
 })();
