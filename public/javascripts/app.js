f8853c5036a12a8bfdc4b64349c58233ac88dd71
sirko <diskozub@hotmail.com>
2017-01-17 09:05:50 +0100

    subscribeuser

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,7 +2,7 @@
     angular.module("myApp")
         .controller('appController', appController);
 
-    function appController($scope, $http, $cookies,$routeParams) {
+    function appController($log,$scope, $http, $cookies,$routeParams) {
     	var vm = this;
     	
     	//preuzmi podatke o aplikaciji
@@ -16,12 +16,64 @@
                 vm.eventsData = response.data.eventData.events;
                 console.log(vm.eventsData);
         });
-    		});
 
-        
+        vm.submit=function(){
+            var data={"user_id":vm.item._id,"app_id":vm.app._id};
+            $http.post("/api/app/subscribe",data,{headers: { 'x-access-token': $cookies.get("token") }}).then(function(response){
+                alert(JSON.stringify(response))
+            });
 
-    	
+        }
 
-    	
+    });
+
+    //za autocomplete    
+    var self = this;
+
+    self.simulateQuery = false;
+    self.isDisabled    = false;
+
+     $http.get("/users/", { headers: { 'x-access-token': $cookies.get("token") } }).then(function (response) {
+        self.repos=response.data;
+     });
+
+    self.querySearch   = querySearch;
+    self.selectedItemChange = selectedItemChange;
+    self.searchTextChange   = searchTextChange;
+
+   
+    function querySearch (query) {
+      var results = query ? self.repos.filter( createFilterFor(query) ) : self.repos,
+          deferred;
+      if (self.simulateQuery) {
+        deferred = $q.defer();
+        $timeout(function () { deferred.resolve( results ); }, Math.random() * 1000, false);
+        return deferred.promise;
+      } else {
+        return results;
+      }
     }
+
+    function searchTextChange(text) {
+      $log.info('Text changed to ' + text);
+    }
+
+    function selectedItemChange(item) {
+      $log.info('Item changed to ' + JSON.stringify(item));
+      vm.item=item;
+    }
+
+    function createFilterFor(query) {
+      var lowercaseQuery = angular.lowercase(query);
+
+      return function filterFn(item) {
+        return (item.username.indexOf(lowercaseQuery) === 0);
+      };
+
+    }
+  }
+    	
+
+    	
+    
 })();
